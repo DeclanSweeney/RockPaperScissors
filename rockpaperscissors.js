1aa15bf4adba8b254da9023d252f347d79824b86
Declan <declan.sweeneyNZDF@gmail.com>
2023-01-19 11:38:57 +1300

    Adds basic gui functionality with working buttons

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,71 +1,83 @@
-const RPS_ARRAY = ["rock", "paper", "scissors"];
+const RPS_ARRAY = ['rock', 'paper', 'scissors'];
+
+document.querySelectorAll('button').forEach((item) => {
+  item.addEventListener('click', (e) => {
+    playRPSRound(item.id);
+  });
+});
 
 function getComputerChoice() {
-    let computerChoice = null;
-    let compInt = Math.floor(Math.random() *3);
-    switch(compInt){
-        case 0:
-            computerChoice = 'rock';
-            break;
-        case 1:
-            computerChoice = 'paper';
-            break;
-        case 2:
-            computerChoice = 'scissors';
-            break;
-    }
-    return computerChoice;
+  let computerChoice = null;
+  let compInt = Math.floor(Math.random() * 3);
+  switch (compInt) {
+    case 0:
+      computerChoice = 'rock';
+      break;
+    case 1:
+      computerChoice = 'paper';
+      break;
+    case 2:
+      computerChoice = 'scissors';
+      break;
+  }
+  return computerChoice;
 }
 
-function sanitizeInput() {
-    let playerChoice = prompt('Rock, Paper or Scissors?');
-    let sanitizedPlayerChoice = playerChoice.toLowerCase();
-    for (i in RPS_ARRAY) {
-        if (RPS_ARRAY[i] == sanitizedPlayerChoice) {
-            return sanitizedPlayerChoice;
-        } else {
-            i++;
-        }
+function sanitizeInput(playerChoice) {
+  let sanitizedPlayerChoice = playerChoice.toLowerCase();
+  for (i in RPS_ARRAY) {
+    if (RPS_ARRAY.includes(sanitizedPlayerChoice)) {
+      return sanitizedPlayerChoice;
+    } else {
+      i++;
     }
-    alert('Please enter a valid response...');
-    return sanitizeInput();
+    return;
+  }
 }
 
 function determineWinner(compChoice, playerChoice) {
-    if (compChoice == playerChoice) {
-        console.log(`Draw! Both players picked ${compChoice}`);
-        return 0;
+  if (compChoice == playerChoice) {
+    console.log(`Draw! Both players picked ${compChoice}`);
+    return 0;
+  } else {
+    if (
+      (compChoice == 'rock' && playerChoice == 'scissors') ||
+      (compChoice == 'paper' && playerChoice == 'rock') ||
+      (compChoice == 'scissors' && playerChoice == 'paper')
+    ) {
+      console.log(
+        `Computer wins! It chose ${compChoice}, while you chose ${playerChoice}`
+      );
+      return 1;
     } else {
-        if ((compChoice == 'rock' && playerChoice == 'scissors')
-        || (compChoice == 'paper' && playerChoice == 'rock') 
-        || (compChoice == 'scissors' && playerChoice == 'paper')) {
-            console.log(`Computer wins! It chose ${compChoice}, while you chose ${playerChoice}`);
-            return 1;
-        } else {
-            console.log(`Player wins! You chose ${playerChoice}, while computer chose ${compChoice}`);
-            return 2;
-        }
+      console.log(
+        `Player wins! You chose ${playerChoice}, while computer chose ${compChoice}`
+      );
+      return 2;
     }
+  }
 }
 
-function playRPSRound() {
-    let compChoice = getComputerChoice();
-    let sanitizedChoice = sanitizeInput();
-    return determineWinner(compChoice, sanitizedChoice);
+function playRPSRound(playerChoice) {
+  let compChoice = getComputerChoice();
+  let sanitizedChoice = sanitizeInput(playerChoice);
+  if (playerChoice == null) return;
+  return determineWinner(compChoice, sanitizedChoice);
 }
 
 let playerScore = 0;
 let compScore = 0;
-while (playerScore < 3 && compScore < 3) {
-    let winner = playRPSRound();
-    switch(winner) {
-        case 1:
-            compScore++;
-            break;
-        case 2:
-            playerScore++;
-            break;
-    }
+while (playerScore < 5 && compScore < 5) {
+  let winner = playRPSRound();
+  switch (winner) {
+    case 1:
+      compScore++;
+      break;
+    case 2:
+      playerScore++;
+      break;
+  }
 }
-console.log(`Game over! Player scored: ${playerScore}, Computer Scored ${compScore}`);
-
+console.log(
+  `Game over! Player scored: ${playerScore}, Computer Scored ${compScore}`
+);
